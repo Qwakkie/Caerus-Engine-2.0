d9219bc7ad9afc3221b099c69678a6b89a32f424
Quinten Bastin <quinten.bastin@student.howest.be>
2021-07-31 19:31:01 +0200

    Scoreboard functionality finished
    only event implementation is left

--- Galaga/Scoreboard.cpp
+++ Galaga/Scoreboard.cpp
@@ -9,18 +9,51 @@
 Scoreboard::Scoreboard()
 	:m_pView(new GameObject())
 {
-	m_pView->GetTransform()->Translate({ 400.f, 0.f, 0.f });
-	
+	m_pView->GetTransform()->Translate({ 500.f, 0.f, 0.f });
+
 	auto* pFont{ ResourceManager::GetInstance().LoadFont("../Resources/ARCADEPI.TTF", 20) };
 
-	auto* pHigh{ new GameObject() };
-	pHigh->AddComponent(new TextureComponent());
+	const float margin{ 20.f };
+	const float heightOffset{ 50.f };
+
+	auto* pHighText{ new GameObject() };
+	pHighText->AddComponent(new TextureComponent());
 	auto* pText{ new TextComponent("HIGH", pFont) };
 	pText->SetColor({ 255, 0, 0 });
-	pHigh->AddComponent(pText);
-	m_pView->AddChild(pHigh);
+	pHighText->AddComponent(pText);
+	pHighText->GetTransform()->Translate({ margin, heightOffset, 0.f });
+	m_pView->AddChild(pHighText);
+
+	auto* pScoreText{ new GameObject() };
+	pScoreText->AddComponent(new TextureComponent());
+	pText = new TextComponent("SCORE", pFont);
+	pText->SetColor({ 255, 0, 0, 1 });
+	pScoreText->AddComponent(pText);
+	pScoreText->GetTransform()->Translate({ margin * 2.f, heightOffset + margin, 0.f });
+	m_pView->AddChild(pScoreText);
+
+	auto* pHighScore{ new GameObject() };
+	pHighScore->AddComponent(new TextureComponent());
+	pText = new TextComponent(std::to_string(m_HighScore), pFont);
+	pHighScore->AddComponent(pText);
+	pHighScore->GetTransform()->Translate({margin * 2.f, heightOffset + margin * 2.f, 0.f});
+	m_pView->AddChild(pHighScore);
+
+	m_pScoreView = new GameObject();
+	m_pScoreView->AddComponent(new TextureComponent());
+	pText = new TextComponent(std::to_string(m_Score), pFont);
+	m_pScoreView->AddComponent(pText);
+	m_pScoreView->GetTransform()->Translate({ margin * 2.f, heightOffset * 3.f, 0.f });
+	m_pView->AddChild(m_pScoreView);
 }
 
 void Scoreboard::Notify(GameObject*, int)
 {
+}
+
+void Scoreboard::AddPoints(unsigned amount)
+{
+	m_Score += amount;
+	auto* pText{ m_pScoreView->GetComponent<TextComponent>() };
+	pText->SetText(std::to_string(m_Score));
 }
--- Galaga/Scoreboard.h
+++ Galaga/Scoreboard.h
@@ -10,6 +10,10 @@ public:
 
 	void Notify(GameObject*, int eventId) override;
 private:
+	void AddPoints(unsigned int amount);
 	GameObject* m_pView;
+	GameObject* m_pScoreView;
+	const int m_HighScore{ 30000 };
+	unsigned int m_Score{};
 };
 
